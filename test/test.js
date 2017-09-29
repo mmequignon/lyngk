aab090c0607f3742c3d78afc31eccc1e1d969a56
Matthieu MÉQUIGNON <matthieu@fwzte.xyz>
2017-09-29 23:59:05 +0200

    [IMP] test.js - Add a test that checks that by default, the state of a coordinate is 'VACANT'

--- test/test.js
+++ test/test.js
@@ -49,3 +49,9 @@ LyngkTestCase.prototype.testStory5 = function() {
 
     assertTrue(coordinates.hash() === 68);
 };
+
+LyngkTestCase.prototype.testStory6 = function() {
+    var coordinates = new Lyngk.Coordinates("A", 3);
+
+    assertTrue(coordinates.get_state() === "VACANT");
+};
