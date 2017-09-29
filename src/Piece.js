0fe6dacda502bac24d964d4690146a906d63c0dc
Matthieu MÉQUIGNON <matthieu@fwzte.xyz>
2017-09-30 01:07:57 +0200

    [IMP] Piece.js - Add the class 'Piece'

--- src/Piece.js
+++ src/Piece.js
@@ -1,4 +1,15 @@
 "use strict";
 
 Lyngk.Piece = function (c) {
+    var private_color;
+
+    var init = function(c){
+        private_color = c;
+    }
+
+    this.get_color = function() {
+        return private_color;
+    };
+
+    init(c);
 };
