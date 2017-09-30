e31efe1ad5043789f21acc1d20f272f4cfc6ecf8
Matthieu MÉQUIGNON <matthieu@fwzte.xyz>
2017-10-01 01:58:40 +0200

    [REF] test.js, Engine.js - Lines and Columns refactoring.

--- src/Engine.js
+++ src/Engine.js
@@ -2,17 +2,17 @@
 
 // enums definition
 Lyngk.Color = {BLACK: 0, IVORY: 1, BLUE: 2, RED: 3, GREEN: 4, WHITE: 5};
+Lyngk.Lines = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+Lyngk.Columns = ["A", "B", "C", "D", "E", "F", "G", "H", "I"];
 
 Lyngk.Engine = function () {
     var private_coordinates = [];
 
     var init = function(){
-        var columns = ["A", "B", "C", "D", "E", "F", "G", "H", "I"];
-        var lines = [1, 2, 3, 4, 5, 6, 7, 8, 9];
         var piece = new Lyngk.Piece("blue");
-        for (var c in columns){
-            for (var l in lines){
-                var coordinate = new Lyngk.Coordinates(c, l);
+        for (var c in Lyngk.Columns){
+            for (var l in Lyngk.Lines){
+                var coordinate = new Lyngk.Coordinates(Lyngk.Columns[c], Lyngk.Lines[l]);
                 if (coordinate.is_valid() === true) {
                     coordinate.put(piece);
                     var pos = coordinate.hash();
