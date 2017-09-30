f2cfe64cb38a7ecf7719a693b6c721094cfd1d22
Matthieu MÉQUIGNON <matthieu@fwzte.xyz>
2017-09-30 12:28:49 +0200

    [IMP] Engine.js - Add game initialisation mechanism.

--- src/Engine.js
+++ src/Engine.js
@@ -4,4 +4,27 @@
 Lyngk.Color = {BLACK: 0, IVORY: 1, BLUE: 2, RED: 3, GREEN: 4, WHITE: 5};
 
 Lyngk.Engine = function () {
+    var private_coordinates = [];
+
+    var init = function(){
+        var columns = ["A", "B", "C", "D", "E", "F", "G", "H", "I"];
+        var lines = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        var piece = new Lyngk.Piece("blue");
+        for (var c in columns){
+            for (var l in lines){
+                var coordinate = new Lyngk.Coordinates(c, l);
+                if (coordinate.is_valid() === true) {
+                    coordinate.put(piece);
+                    var pos = coordinate.hash();
+                    private_coordinates[pos] = coordinate;
+                }
+            }
+        }
+    };
+
+    this.get_coordinates = function() {
+        return private_coordinates;
+    };
+
+    init();
 };
