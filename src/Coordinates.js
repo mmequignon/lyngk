c7a55da2612fb681f1e968420f40dd39f48083ad
Matthieu MÉQUIGNON <matthieu@fwzte.xyz>
2017-09-30 02:23:16 +0200

    [IMP] Coordinates.js - Beautify array

--- src/Coordinates.js
+++ src/Coordinates.js
@@ -5,7 +5,15 @@ Lyngk.Coordinates = function (c, l) {
 
     this.is_valid = function(){
 
-        var not_valid_positions = ["A1", "A2", "A4", "A5", "A6", "A7", "A8", "A9", "B1", "B6", "B7", "B8", "B9", "C8", "C9", "D1", "D8", "D9","E1", "E9", "F1", "F2", "F9", "G1", "G2", "H1", "H2", "H3", "H4", "H9", "I1", "I2", "I3", "I4", "I5", "I6", "I8", "I9"];
+        var not_valid_positions = ["A1", "A2", "A4", "A5", "A6", "A7", "A8", "A9",
+                                   "B1", "B6", "B7", "B8", "B9",
+                                   "C8", "C9",
+                                   "D1", "D8", "D9",
+                                   "E1", "E9",
+                                   "F1", "F2", "F9",
+                                   "G1", "G2",
+                                   "H1", "H2", "H3", "H4", "H9",
+                                   "I1", "I2", "I3", "I4", "I5", "I6", "I8", "I9"];
         return (not_valid_positions.indexOf(private_column + private_line) < 0);
     };
 
