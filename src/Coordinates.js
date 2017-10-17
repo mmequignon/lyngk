4d2666cf02dff78c7868af162e5ec0a11f28597b
Matthieu MÉQUIGNON <matthieu@fwzte.xyz>
2017-10-17 14:25:37 +0200

    [REF] Modify implementation of intersections. Now, there is a new attribute that is a list of pieces.

--- src/Coordinates.js
+++ src/Coordinates.js
@@ -1,7 +1,7 @@
 "use strict";
 
 Lyngk.Coordinates = function (c, l) {
-    var private_column, private_line, private_state, private_color, private_count;
+    var private_column, private_line, private_state, private_color, private_count, private_pieces;
 
     this.is_valid = function(){
 
@@ -22,6 +22,7 @@ Lyngk.Coordinates = function (c, l) {
         private_line = l;
         private_state = "VACANT";
         private_count = 0;
+        private_pieces = [];
     };
 
     this.representation = function(){
@@ -41,31 +42,36 @@ Lyngk.Coordinates = function (c, l) {
     };
 
     this.get_state = function(){
-        return private_state;
+        if (private_pieces.length === 0){
+            return "VACANT";
+        }
+        else if (private_pieces.length === 5){
+            return "FULL_STACK";
+        }
+        else if (private_pieces.length === 1){
+            return "ONE_PIECE";
+        }
+        else{
+            return "STACK";
+        }
     };
 
     this.get_color = function(){
-        return private_color;
+        return private_pieces[private_pieces.length - 1].get_color();
     };
 
     this.get_count = function(){
-        return private_count;
+        return private_pieces.length;
     };
 
     this.put = function(piece) {
-        if (private_state !== "FULL_STACK"){
-            if (private_count === 0){
-                private_state = "ONE_PIECE";
-            }
-            else if (private_count === 4){
-                private_state = "FULL_STACK";
-            }
-            else {
-                private_state = "STACK";
-            }
-            private_color = piece.get_color();
-            private_count += 1;
+        if (this.get_state() !== "FULL_STACK"){
+            private_pieces.push(piece);
         }
+    };
+
+    this.get_pieces = function(){
+        return private_pieces;
     };
 
     init(c, l);
